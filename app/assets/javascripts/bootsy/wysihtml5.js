504704f013f7974be05dffcb6a0d454d7ce216a7
benhill <benhill@gmail.com>
2014-10-26 11:44:58 -0700

    adding class to header

--- app/assets/javascripts/bootsy/wysihtml5.js
+++ app/assets/javascripts/bootsy/wysihtml5.js
@@ -8309,6 +8309,7 @@ wysihtml5.views.View = Base.extend(
       ADDITIONAL_CSS_RULES = [
         "html             { height: 100%; }",
         "body             { min-height: 100%; padding: 0; margin: 0; margin-top: -1px; padding-top: 1px; }",
+        ".wysiwyg-float-left {float: left;margin: 0 8px 8px 0;}",
         "._wysihtml5-temp { display: none; }",
         wysihtml5.browser.isGecko ?
           "body.placeholder { color: graytext !important; }" :
